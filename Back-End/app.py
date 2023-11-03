68be963580e38cb497ecbb0f860ece3af86e2e9c
ma-brito <miki922010@hotmail.com>
2023-11-02 19:59:19 -0600

    conexión front-end con back-end, conexión base datos, flujos alternativos y excepcionales

--- Back-End/app.py
+++ Back-End/app.py
@@ -2,52 +2,85 @@ import json
 import os
 
 from alchemyClasses import db
-from CryptoUtils.CryptoUtils import validate
+from alchemyClasses.Usuario import Usuario
+from CryptoUtils.CryptoUtils import validate, cipher, decipher
 from flask import Flask, render_template, request, flash, session, g, redirect, url_for
-
 from controllers.JsonController import json_controller
 from model.model_pelicula import get_movie_by_id
 from model.model_renta import rents_by_email
-from model.model_usuario import get_user_by_email, get_user_by_id
-
+from model.model_usuario import get_user_by_email, get_user_by_name
+from flask import Blueprint, render_template, jsonify
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 app.register_blueprint(json_controller)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://ferfong:Developer123!@localhost:3306/ing_soft"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://d:Developer123!@localhost:3306/ing_soft"
 app.config.from_mapping(
     SECRET_KEY='dev',
 )
+CORS(app)
 db.init_app(app)
 
+@cross_origin
 @app.route('/', methods=['GET', 'POST'])
 def main():
     return redirect(url_for('login'))
 
+@cross_origin
+@app.route('/registrar', methods=["POST"])
+def registrar():
+    email = request.json["email"]
+    password = request.json["password"]
+    nombre = request.json["nombre"]
+
+    if get_user_by_email(email) != []:
+        return jsonify({"error": "El correo ingresado ya está en uso."})
+    else:
+        usuario = Usuario(email = email, nombre = nombre, password = password) 
+        db.session.add(usuario)
+        db.session.commit()
+
+        return jsonify({
+            "email": usuario.email,
+            "password": usuario.password
+        })
+
+@cross_origin
+@app.route("/@me")
+def get_current_user():
+    user_id = session.get("email", None)
+
+    if not user_id:
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    user = get_user_by_email(user_id)[0]
+    print(user)
+    return jsonify({
+        "email": user.email,
+        "password": user.password
+    }) 
+
+
+@cross_origin
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if session.get("user", None) is not None and request.method == 'GET':
         return redirect(url_for('index'))
-    if request.method == 'POST':
-        try:
-            email = request.form.get('email')
-            passwd = request.form.get('passwd')
-            user_query = get_user_by_email(email)
-            if not user_query:
-                flash('Ese correo no existe.')
-                return render_template('login.html')
-            user = user_query[0]
-            if not validate(passwd, user.password):
-                flash('Contraseña incorrecta')
-                return render_template('login.html')
-            session.clear()
-            session['user']= user.nombre
-            session['email']= user.email
-            session.modified = True
-            return render_template('index.html')
-        except KeyError:
-            flash('No fue enviado con éxito el correo y/o la contraseña')
-            return render_template('login.html')
-    return render_template('login.html')
-
+    email = request.json["email"]
+    password = request.json["password"]
+    user_query = get_user_by_email(email)
+    if not user_query:
+        return jsonify({"error": "El correo ingresado no está registrado."}), 401 
+    user = user_query[0]
+    if not validate(password, user.password):
+        return jsonify({"error": "Contraseña incorrecta."}), 402
+    session.clear()
+    session['user']= user.nombre
+    session['email']= user.email
+    session.modified = True
+    return jsonify({
+        "email": user.email,
+        "password": user.password
+    })
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
@@ -65,4 +98,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
